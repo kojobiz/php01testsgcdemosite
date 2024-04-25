f3e850439232e22d062554e76c76a948f5f26b50
小城 <kojolumi@gamil.com>
2024-04-25 18:00:36 +0900

    test

--- 404.php
+++ 404.php
@@ -2,14 +2,14 @@
 /**
  * The template for displaying 404 pages (not found).
  *
- * @package Sydney
+ * @package demosite_theme
  */
 
 get_header(); ?>
 
 	<div id="primary" class="content-area col-md-12">
 		<main id="main" class="site-main" role="main">
-			<?php do_action( '404エラー' ); ?>
+			<?php do_action( 'demosite_theme_404_content' ); ?>
 		</main><!-- #main -->
 	</div><!-- #primary -->
 
